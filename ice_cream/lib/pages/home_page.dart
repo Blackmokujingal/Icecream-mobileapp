eff621261b28ba9426db51a4dc130e52357728cd
Blackmokujingal <bea800.w98@gmail.com>
2024-01-13 09:01:21 +0400

    Home page V1

---
+++ ice_cream/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+// home_page.dart
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Your homepage implementation
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
+    );
+  }
+}
